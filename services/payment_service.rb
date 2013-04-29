a7d86cf9a358ef3c78e64cffd1422944f969d8b0
Kylie Jo <kjinfield@gmail.com>
2013-04-28 21:39:29 -0500

    Make service work make it host on orthrus on port 8081 other edits to run as service edit command to run properly

--- services/payment_service.rb
+++ services/payment_service.rb
@@ -1,49 +1,59 @@
-class XpCheckServer < SOAP::RPC::StandaloneServer
-	def initialize (*args)
-		add_method(self, 'credit_check', 'user', 'cost', 'item', 'quantity')
-	end
+require "soap/rpc/standaloneserver"
 
-	def credit_check (user, cost, item, quantity)	
-		require "zlib"
+begin
+	class XpCheckServer < SOAP::RPC::StandaloneServer
+		def initialize (*args)
+			add_method(self, 'credit_check', 'user', 'cost', 'item', 'quantity')
+		end
 
-		begin
-			#read in <user>.dat file and parse for XpLevel (58704c6576656c)
-			# after XpLevel is found, the 2nd and 3rd following bytes represent the level
-			file = Zlib::GzipReader.new(open("#{user}.dat"))
-			rite_bites = [0x58, 0x70, 0x4c, 0x65, 0x76, 0x65, 0x6c, 0x00]
+		def credit_check (user, cost, item, quantity)	
+			require "zlib"
 
-			rb_counter = 0
-			level_byte_index = 0
-			level_byte = 0
-			file.each_byte do |byte|
-				if rb_counter == rite_bites.length then
-					level_byte_index += 1
-					if level_byte_index == 3 then
-						level_byte = byte
-						break
+			begin
+				#read in <user>.dat file and parse for XpLevel (58704c6576656c)
+				# after XpLevel is found, the 2nd and 3rd
+				# following bytes represent the level
+				file = Zlib::GzipReader.new(open("#{user}.dat"))
+				rite_bites = [0x58, 0x70, 0x4c, 0x65, 0x76, 0x65, 0x6c, 0x00]
+
+				rb_counter = 0
+				level_byte_index = 0
+				level_byte = 0
+				file.each_byte do |byte|
+					if rb_counter == rite_bites.length then
+						level_byte_index += 1
+						if level_byte_index == 3 then
+							level_byte = byte
+							break
+						end
+					elsif byte == rite_bites[rb_counter] then
+						rb_counter += 1	
+					else
+						rb_counter = 0
 					end
-				elsif byte == rite_bites[rb_counter] then
-					rb_counter += 1	
-				else
-					rb_counter = 0
 				end
-			end
 
-			if level_byte >= cost then
-				if take_exp(user, cost, quantity) then
-					#call drew baby ;)
+				if level_byte >= cost then
+					if take_exp(user, cost, quantity) then
+						#call drew baby ;)
+					end
+				else
+					return false
 				end
-			else
+
+			rescue
 				return false
 			end
-
-		rescue
-			return false
+		end
+			
+		def take_exp (user, cost, quantity)	
+			#if user has enough experience, then remove required cost
+			return system('su minecraft bash -c "screen -p 0 -S TekkitServer -X eval \'stuff \"$(eval echo "xp -#{amount}L #{user}")\"\015\'"')
 		end
 	end
-		
-	def take_exp (user, cost, quantity)	
-		#if user has enough experience, then remove required cost
-		return `su minecraft bash -c "screen -p 0 -S TekkitServer -X eval 'stuff \"$(eval echo "xp -#{amount}L #{user}")\"\015'"`
-	end
+
+	XpCheck = XpCheckServer.new("CreditChecker",
+		urn:ruby:CreditChecker, orthrus.kyliejo.com, 8081)
+rescue => err
+	puts err.message
 end
