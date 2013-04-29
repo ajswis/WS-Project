52d53b1e71a0d468a8c062c0ba2630277441e61a
Drew <bs.of.dth@gmail.com>
2013-04-29 16:12:12 -0500

    Fix bug concerning wrong variable usage.

--- services/XpCheckServer.rb
+++ services/XpCheckServer.rb
@@ -18,7 +18,7 @@ begin
         # Read in <user>.dat file and parse for XpLevel (58704c6576656c)
         # after XpLevel is found, the 2nd and 3rd
         # following bytes represent the level
-        file = Zlib::GzipReader.new(open("/home/minecraft/TekkiteServer/player/#{user}.dat"))
+        file = Zlib::GzipReader.new(open("/home/minecraft/TekkitServer/players/#{user}.dat"))
         rite_bites = [0x58, 0x70, 0x4c, 0x65, 0x76, 0x65, 0x6c, 0x00]
 
         rb_counter = 0
@@ -47,23 +47,26 @@ begin
               return driver.distribute(user, item, quantity)
             rescue
               # Error calling the next service function
+              puts "Failed to call distribution service."
               return false
             end
           end
         else
           # Not enough levels to fund item purchase
+          puts "Not enough levels."
           return false
         end
 
-      rescue
+      rescue => err
         # Something went wrong find available levels.
+		    puts err.message
         return false
       end
     end
 
     def take_exp (user, cost, quantity)
       #if user has enough experience, then remove required cost
-      partial = "\\\"$(eval echo \"xp -#{amount}L #{user}\")\\\""
+      partial = "\\\"$(eval echo \"xp -#{cost}L #{user}\")\\\""
       return system("sudo su minecraft bash -c \"screen -p 0 -S TekkitServer -X eval 'stuff "+partial+"\\015'\"")
     end
   end
