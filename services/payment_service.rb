c52e21f059c8742e59581ff8bfb2de5279eb7a32
Kylie Jo <kjinfield@gmail.com>
2013-04-28 20:44:57 -0500

    implement as server make methods to check credit and take experience return if it fails

--- services/payment_service.rb
+++ services/payment_service.rb
@@ -1,34 +1,45 @@
-user = "kjinfield"
-amount = 1
+class XpCheckServer < SOAP::RPC::StandaloneServer
+	def credit_check (user, cost, item, quantity)	
+		require "zlib"
 
-require "zlib"
+		begin
+			#read in <user>.dat file and parse for XpLevel (58704c6576656c)
+			# after XpLevel is found, the 2nd and 3rd following bytes represent the level
+			file = Zlib::GzipReader.new(open("#{user}.dat"))
+			rite_bites = [0x58, 0x70, 0x4c, 0x65, 0x76, 0x65, 0x6c, 0x00]
 
-begin
-	file = Zlib::GzipReader.new(open("#{user}.dat"))
-	rite_bites = [0x58, 0x70, 0x4c, 0x65, 0x76, 0x65, 0x6c, 0x00]
-
-	rb_counter = 0
-	level_byte_index = 0
-	level_byte = 0
-	file.each_byte do |byte|
-		if rb_counter == rite_bites.length then
-			level_byte_index += 1
-			if level_byte_index == 3 then
-				level_byte = byte
-				break
-			end
-		elsif byte == rite_bites[rb_counter] then
-			rb_counter += 1	
-		else
 			rb_counter = 0
+			level_byte_index = 0
+			level_byte = 0
+			file.each_byte do |byte|
+				if rb_counter == rite_bites.length then
+					level_byte_index += 1
+					if level_byte_index == 3 then
+						level_byte = byte
+						break
+					end
+				elsif byte == rite_bites[rb_counter] then
+					rb_counter += 1	
+				else
+					rb_counter = 0
+				end
+			end
+
+			if level_byte >= cost then
+				if take_exp(user, cost, quantity) then
+					#call drew baby ;)
+				end
+			else
+				return false
+			end
+
+		rescue
+			return false
 		end
 	end
-
-	if level_byte >= amount then
-		puts "true"
-		#if user has enough experience, then remove required amount
-		#{}`sudo su minecraft bash -c "screen -p 0 -S TekkitServer -X eval 'stuff \"$(eval echo "xp -#{amount}L #{user}")\"\015'"`
+		
+	def take_exp (user, cost, quantity)	
+		#if user has enough experience, then remove required cost
+		return `su minecraft bash -c "screen -p 0 -S TekkitServer -X eval 'stuff \"$(eval echo "xp -#{amount}L #{user}")\"\015'"`
 	end
-rescue
-	puts "false"
 end
