4e288d78038c0ef827381c9c63d1159b844caefa
Kenneth <kenneth@Dusty.(none)>
2013-04-28 21:56:49 -0500

    Rearraged and added a main, initialized method and attempted class. Doesn't run tho, sadly

--- services/selection.rb
+++ services/selection.rb
@@ -1,79 +1,106 @@
 #!/usr/bin/ruby
+require "soap/rpc/StandaloneServer"
 
-def UserInput(filename)
-
-	file = File.open(filename, "r")
-	ids = file.read.split(" ")
-
-	username = ids[0]
-	item = ids[1]
-	amount = ids[2]
-
-	item.downcase!
-
-	return username, item, amount
-end
-
-
-
-
-
-def GetIds(filename)
-	file = File.open(filename, "r")
-	ids = file.read.split("\n")
-
-	(0..ids.length-1).each do |i|
- 	  ids[i] = ids[i].split(" ")
-	end
-
-	return ids
-end 
-
-
-
-
-
-def findIdCost(item, ids, amount)
-	itemID = 0
-	cost = 0
-
-	(0..ids.length-1).each do |i|
-		ids[i][0].downcase! 
-   		if item == ids[i][0] then
-   			itemID = ids[i][1]
-			cost = ids[i][2]
+begin
+	class ItemSelection < SOAP::RPC::StandaloneServer
+		
+		def initialize (*args)
+			add_method(self, 'main', args)
 		end
+
+
+
+		username = " "
+		item = " "
+		amount = " "
+		StackValue = 64.0
+
+
+
+
+
+		def main(string)	
+
+			username, item, amount = UserInput(string)
+			ids = GetIds("Ids64.txt")
+			itemID, cost = findIdCost(item, ids, amount)
+
+			return username, itemID, amount, cost
+		end
+
+
+
+
+
+		def UserInput(filename)
+
+			file = File.open(filename, "r")
+			ids = file.read.split(" ")
+
+			username = ids[0]
+			item = ids[1]
+			amount = ids[2]
+
+			item.downcase!
+
+			return username, item, amount
+		end
+
+
+
+
+
+		def GetIds(filename)
+			file = File.open(filename, "r")
+			ids = file.read.split("\n")
+
+			(0..ids.length-1).each do |i|
+		 	  ids[i] = ids[i].split(" ")
+			end
+
+			return ids
+		end 
+
+
+
+
+
+		def findIdCost(item, ids, amount)
+			itemID = 0
+			cost = 0
+
+			(0..ids.length-1).each do |i|
+				ids[i][0].downcase! 
+		   		if item == ids[i][0] then
+		   			itemID = ids[i][1]
+					cost = ids[i][2]
+				end
+			end
+
+			if itemID == 0 or cost == 0 then
+				#Error Code
+			end
+
+			cost =  cost.to_i / (StackValue / amount.to_i)
+			if cost == 0
+				cost = 1
+			end
+
+			cost = cost.round(0)
+
+			return itemID, cost
+		end
+
+
+
+
+		username, itemID, amount, cost = main("Request.txt")
+
+		puts username
+		puts itemID
+		puts amount
+		puts cost
 	end
-
-	if itemID == 0 or cost == 0 then
-		#Error Code
-	end
-
-	cost =  cost.to_i / (StackValue / amount.to_i)
-	if cost == 0
-		cost = 1
-	end
-
-	cost = cost.round(0)
-
-	return itemID, cost
+rescue
 end
-
-
-
-
-
-username = " "
-item = " "
-amount = " "
-StackValue = 64.0
-
-username, item, amount = UserInput("Request.txt")
-ids = GetIds("Ids64.txt")
-itemID, cost = findIdCost(item, ids, amount)
-
-puts username
-#puts item
-puts itemID
-puts amount
-puts cost
+#c = ItemSelection.new
